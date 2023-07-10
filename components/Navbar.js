8436e75513e93a7e61c6f27ee41f85837320e8bb
hammad <hammadqurashi103@gmail.com>
2023-07-10 15:01:26 +0500

    fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import TopNavbar from "./NavbarComponents/TopNavbar";
 import FloatingNavBar from "./NavbarComponents/FloatingNavBar";
 
 const getCategories = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getcategories`, {
+  const res = await fetch(`${process.env.HOST}/api/getcategories`, {
     cache: "no-store",
   });
   return await res.json();
